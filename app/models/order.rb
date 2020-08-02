b02609dc659afe745f19238d3bb70320fcc0aef9
Raven Carlos T. Rodrigo <ravencarlostrodrigo@gmail.com>
2020-08-02 20:15:51 +0800

    Test validation order uuid callbacks with spec

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,23 @@ class Order < ApplicationRecord
   belongs_to :warehouse
 
   validates :customer_name, presence: true
+
+  before_validation :assign_uuid
+
+  private
+
+  def assign_uuid
+    return unless new_record? || uuid.empty?
+
+    generated_uuid = ''
+    presence = true
+
+    while presence == true
+      generated_uuid = SecureRandom.uuid
+
+      presence = false unless Order.exists?(uuid: generated_uuid)
+    end
+
+    self.uuid = generated_uuid
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe Order, type: :model do
 
     it { is_expected.to validate_presence_of(:customer_name) }
   end
+
+  describe 'callbacks' do
+    describe 'before_validation' do
+      describe '#assign_uuid' do
+        it 'assigns a randomly generated uuid' do
+          order = build(:order, uuid: nil)
+
+          order.valid?
+
+          expect(order.uuid.present?).to eq(true)
+        end
+      end
+    end
+  end
 end
