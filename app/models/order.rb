619a54a0d97549d4b670a3bbc85026e243841fed
Raven Carlos T. Rodrigo <ravencarlostrodrigo@gmail.com>
2020-08-02 20:04:06 +0800

    Test validation of order model with spec

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :warehouse
+
+  validates :customer_name, presence: true
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    subject(:order) { build(:order) }
+
+    it { is_expected.to validate_presence_of(:customer_name) }
+  end
 end
