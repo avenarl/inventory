9e873715bc5089503bc0bbf22979f7db539e19f1
Raven Carlos T. Rodrigo <ravencarlostrodrigo@gmail.com>
2020-08-06 15:24:04 +0800

    Add params method in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,13 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  # Private methods means that it can't be accessed by users
+  # Not accessible via any routes
+  private
+
+  # Method parameters: GET / POST request
+  def products_params
+    params.require(:product).permit(:name, :sku)
+  end
 end
