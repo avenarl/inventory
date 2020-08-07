b94040e039edf9dd8f970e51c9c051b12f2c227d
Raven Carlos T. Rodrigo <ravencarlostrodrigo@gmail.com>
2020-08-07 22:23:56 +0800

    Add new and create method for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,20 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+  
+  def new 
+    @product = Product.new
+  end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash.notice = 'Successfully created a product!'
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
   # Private methods means that it can't be accessed by users
   # Not accessible via any routes
   private
