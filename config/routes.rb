c2e44127124d305eb3c48775bb112112f8bbf5e2
Raven Carlos T. Rodrigo <ravencarlostrodrigo@gmail.com>
2020-08-08 21:20:08 +0800

    Add routes products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :products, only: %i[index new]
+  resources :products, only: %i[index show edit destroy new create]
   devise_for :users
   root to: "pages#homepage"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
