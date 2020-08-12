65aa227249cd813a263e597a86b81d7ec49ab926
Raven Carlos T. Rodrigo <ravencarlostrodrigo@gmail.com>
2020-08-12 20:51:10 +0800

    Add orders controller with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :products, only: %i[index show edit destroy new create]
+  resources :orders, only: %i[index show create new]
   devise_for :users
   root to: "pages#homepage"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
