366883ab1c13c658d3474a051fa8870b2838ef62
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-11-28 00:23:30 +0800

    :up: update(repository): change method name

--- src/main/java/com/avenarl/inventory/repository/ProductRepository.java
+++ src/main/java/com/avenarl/inventory/repository/ProductRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Optional<Product> findByProductId(Long id);
+    Optional<Product> findById(Long id);
     Optional<Product> findByName(String name);
 
 }
