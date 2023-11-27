043b2bd1f2705f45a00450eb826e13051d1bbf48
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-11-27 23:21:53 +0800

    :heavy_plus_sign: new(service): add product implementation

---
+++ src/main/java/com/avenarl/inventory/service/impl/ProductServiceImpl.java
@@ -0,0 +1,18 @@
+package com.avenarl.inventory.service.impl;
+
+import com.avenarl.inventory.model.Product;
+import com.avenarl.inventory.repository.ProductRepository;
+import com.avenarl.inventory.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
