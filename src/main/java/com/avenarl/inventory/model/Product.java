37c2b0e4394af4085c3437f46c1d4fe8a94a769a
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-11-27 23:46:54 +0800

    :up: update(model): generate construtor product

--- src/main/java/com/avenarl/inventory/model/Product.java
+++ src/main/java/com/avenarl/inventory/model/Product.java
@@ -13,6 +13,11 @@ public class Product {
     @Column
     private String name;
 
+    public Product(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
