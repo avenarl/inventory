d94b8543495e6d36fdbcacd6e390d53f04d19043
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-11-27 21:28:49 +0800

    :heavy_plus_sign: new(model): add product

--- src/main/java/com/avenarl/inventory/model/Product.java
+++ src/main/java/com/avenarl/inventory/model/Product.java
@@ -1,4 +1,15 @@
 package com.avenarl.inventory.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="product", schema="public")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
 }
