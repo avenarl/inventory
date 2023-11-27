7c9fc0e524165a78890ad29c51f4003407bf2e33
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-11-27 23:44:30 +0800

    heavy_plus_sign: new(controller): add product controller

---
+++ src/main/java/com/avenarl/inventory/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.avenarl.inventory.controller;
+
+import com.avenarl.inventory.repository.ProductRepository;
+import com.avenarl.inventory.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/product")
+public class ProductController {
+
+    @Autowired
+    public ProductService productService;
+
+    @Autowired
+    public ProductRepository productRepository;
+
+}
