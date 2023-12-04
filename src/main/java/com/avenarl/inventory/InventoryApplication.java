5a64cb3322f2e5ab6470a4b448f10303174c4e32
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-12-04 16:01:11 +0800

    :up: update(main): add cors configuration

--- src/main/java/com/avenarl/inventory/InventoryApplication.java
+++ src/main/java/com/avenarl/inventory/InventoryApplication.java
@@ -2,12 +2,33 @@ package com.avenarl.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class InventoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedHeaders("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowCredentials(true)
+						.maxAge(3600);
+			}
+		};
 	}
 
 }
