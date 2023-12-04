bf063a2b6a3afdbab9115c97108098b6851280e6
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-12-04 09:39:57 +0800

    :up: update(client): fix subscribe error

--- src/main/client/src/app/feature/product/product-list/product-list.component.ts
+++ src/main/client/src/app/feature/product/product-list/product-list.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/model/product.model';
+import { ProductService } from 'src/app/service/product/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(){}
+  constructor(private productService: ProductService){}
 
   ngOnInit(): void {
-  
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getProducts().subscribe((products: Product[]) => {
+      this.products = products;
+    })
   }
 }
--- src/main/client/src/app/service/product/product.service.ts
+++ src/main/client/src/app/service/product/product.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class ProductService {
 
-  private apiUrl = 'http://localhost:8080/api/v1/tweet';
+  private apiUrl = 'http://localhost:8080/api/v1/product';
 
   constructor(private http: HttpClient) { }
 
