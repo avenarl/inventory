cddc2bd56d2283b4adbfa5652a2b916eaf272189
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-12-04 09:45:16 +0800

    :up: update(client): import http client module

--- src/main/client/src/app/app.module.ts
+++ src/main/client/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './feature/product/product-list/product-list.component';
+import { ProductService } from './service/product/product.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
