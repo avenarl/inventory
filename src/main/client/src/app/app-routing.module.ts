2b4f9761c0b8096a7a8fba6ade272cdf9d0bf85e
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-12-04 09:47:27 +0800

    :up: update(client): add routes product list

--- src/main/client/src/app/app-routing.module.ts
+++ src/main/client/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './feature/product/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
