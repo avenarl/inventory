53e7f3b1fbe6ee74ff5e5e4d49b018240f4c1f49
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-12-04 16:13:27 +0800

    :up: update(routing): modify path

--- src/main/client/src/app/app-routing.module.ts
+++ src/main/client/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './feature/product/product-list/product-list.component';
 
 const routes: Routes = [
-  { path: '', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
 ];
 
 @NgModule({
