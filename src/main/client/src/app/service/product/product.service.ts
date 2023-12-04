d2d3e6dbf37d8e7e0d742e0bfdd48b2e5556171b
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-12-04 09:30:16 +0800

    :up: update(client): add get product method

--- src/main/client/src/app/service/product/product.service.ts
+++ src/main/client/src/app/service/product/product.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Product } from 'src/app/model/product.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/v1/tweet';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiUrl}/products`);
+  }
 }
