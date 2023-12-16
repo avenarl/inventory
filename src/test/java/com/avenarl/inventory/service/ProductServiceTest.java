254bc64122bec01215d55965168e0dc78903e855
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-12-16 16:55:37 +0800

    :white_check_mark: test(service): method add product

--- src/test/java/com/avenarl/inventory/service/ProductServiceTest.java
+++ src/test/java/com/avenarl/inventory/service/ProductServiceTest.java
@@ -1,4 +1,23 @@
 package com.avenarl.inventory.service;
 
+import com.avenarl.inventory.model.Product;
+import com.avenarl.inventory.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.given;
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Test
+    public void addProduct() {
+        Product product = new Product();
+        product.setId(2L);
+        product.setName("Test Product");
+    }
 }
