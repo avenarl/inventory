bd36d8e89beb7688ff494db5dd32cd918df61b50
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-12-20 22:47:03 +0800

    :white_check_mark: test(service): method it should add product

--- src/main/client/src/app/service/product/product.service.spec.ts
+++ src/main/client/src/app/service/product/product.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ProductService } from './product.service';
+import { Product } from 'src/app/model/product.model';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductService]
+    });
     service = TestBed.inject(ProductService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should add a product', () => {
+    const partialProduct: Partial<Product> = {
+      name: 'Product 1',
+    };
+    
+    const fullProduct: Product = {
+      id: 1, 
+      isEditing: false,
+      name: partialProduct.name as string, 
+    };
+    
+    service.addProduct(partialProduct).subscribe(data => {
+      expect(data).toEqual(fullProduct);
+    });
+    
+    const req = httpTestingController.expectOne('http://localhost:8080/api/v1/product/products');
+    expect(req.request.method).toEqual('POST');
+    req.flush(fullProduct);
   });
 });
+
