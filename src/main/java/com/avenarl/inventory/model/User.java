c8b68c71169859dd174e4b762e22db58e0c1c4a6
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-12-23 22:46:57 +0800

    :up: update(model): generate user getter and setter

--- src/main/java/com/avenarl/inventory/model/User.java
+++ src/main/java/com/avenarl/inventory/model/User.java
@@ -21,4 +21,28 @@ public class User {
         this.username = username;
         this.password = password;
     }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
