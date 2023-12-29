98fe40e5787a625f2a1a5856b1d214422a8d56bc
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-12-29 22:16:36 +0800

    :up: update(model): add variable price in product class

--- src/main/client/src/app/model/product.model.ts
+++ src/main/client/src/app/model/product.model.ts
@@ -1,10 +1,12 @@
 export class Product {
     id: number;
     name: string;
+    price: number;
     isEditing?: boolean = false;
     
-constructor(id: number, name: string) {
+constructor(id: number, name: string , price: number) {
     this.id = id;
     this.name = name; 
+    this.price = price;
   }
 }
